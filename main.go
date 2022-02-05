6d5f53c776c2ba74842037384910f41cd20d40dc
lakshays <lakshays@adobe.com>
2022-02-05 07:57:37 -0600

    refactor: adding new structs

--- main.go
+++ main.go
@@ -2,32 +2,46 @@ package main
 
 import (
 	"encoding/xml"
-	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
-var wpSitemap string = "https://www.washingtonpost.com/wp-stat/sitemaps/index.xml"
-
-type Location struct {
-	Loc string `xml:"loc"`
-}
+var wpSitemap = `
+<sitemapindex>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/politics.xml
+</loc>
+</sitemap>
+<sitemap>
+<loc>
+https://www.washingtonpost.com/news-sitemaps/opinions.xml
+</loc>
+</sitemap>
+</sitemapindex>
+`
 
 type SitemapIndex struct {
-	Locations []*Location `xml:"sitemap"`
+	Locations []string `xml:"sitemap>loc"`
 }
 
-func (l *Location) String() string {
-	return l.Loc
+type News struct {
+	Titles    []string `xml:"url>news>titile"`
+	Keywords  []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
 }
 
 func main() {
-	resp, _ := http.Get(wpSitemap)
-	bytes, _ := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
-
 	s := &SitemapIndex{}
-	xml.Unmarshal(bytes, s)
+	n := &News{}
+	xml.Unmarshal([]byte(wpSitemap), s)
 
-	fmt.Println(s.Locations)
+	for _, Location := range s.Locations {
+		Location = strings.TrimSpace(Location)
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		xml.Unmarshal(bytes, &n)
+	}
 }
